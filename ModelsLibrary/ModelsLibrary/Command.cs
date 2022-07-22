a5a6954134df5e151ff35b8dbef7bb8114deaacd
Navatusein <boghdan.kutsulima@gmail.com>
2022-07-23 00:07:29 +0300

    Added new command type: New Chat.

--- ModelsLibrary/ModelsLibrary/Command.cs
+++ ModelsLibrary/ModelsLibrary/Command.cs
@@ -14,7 +14,8 @@ namespace ModelsLibrary
         SyncChatMessage,
         Exception,
         RequestChanges,
-        NewMessage
+        NewMessage,
+        NewChat
     }
 
     [Serializable]
